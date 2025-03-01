9c2578e8cf95754b8fdb0e1f09902896e378e1af
meena76710 <meenalocharla8562@gmail.com>
2025-03-01 13:04:45 +0530

    init

---
+++ world/src/world/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package world.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("hi I'm Reena");
+		System.out.println("Good morning to all....");
+
+	}
+
+}
